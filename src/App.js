7d6e80da5537518fceaa9abf56a8263fa72cc4a7
arish-mhrjn <arish_mhrzn2004@gmail.com>
2024-10-24 14:46:44 +0000

    Body work frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 import React from "react";
 function App() {
     return (
         <div className="app">
             <Header></Header>
+            <Home></Home>
         </div>
         // Header
 
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img
+                    src="https://store-images.s-microsoft.com/image/apps.24594.13510798887500496.393115ce-aadd-41b0-a06b-6de8b907aa10.b4898b91-921e-43ef-aedc-8a0e423c95d5?mode=scale&q=90&h=1080&w=1920"
+                    alt=""
+                    className="home__image"
+                />
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                </div>
+                <div className="home__row">
+                    {/* {Product} */}
+                    {/* {Product} */}
+                    {/* {Product} */}
+                </div>
+                <div className="home__row">{/* {Product} */}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
