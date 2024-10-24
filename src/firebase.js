27012e37bbdb99e5634a1237fe32b6cc664df982
arish-mhrjn <arish_mhrzn2004@gmail.com>
2024-10-24 14:11:58 +0000

    header navBar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCMWHkdgNRJ1ud5FO3kKxjHjQeZP6nuJi4",
+    authDomain: "run-853c8.firebaseapp.com",
+    projectId: "run-853c8",
+    storageBucket: "run-853c8.appspot.com",
+    messagingSenderId: "599420900057",
+    appId: "1:599420900057:web:e663c3bd4baf2e5933e4c8",
+    measurementId: "G-L8119FD1MQ"
+  };
